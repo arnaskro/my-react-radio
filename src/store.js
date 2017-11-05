52a52e3b53c3c35a47649f2cf130d963212b0bfb
Arnas Kromelis <arnas.kromelis@gmail.com>
2017-11-05 16:25:40 +0100

    new radio station

--- src/store.js
+++ src/store.js
@@ -4,6 +4,7 @@ const initialState = {
   list: [
     { title: "European Hit Radio", url: "http://84.46.162.57:8000/ehr_m" },
     { title: "Power Hit Radio", url: "https://power-stream.tv3.lt:8080/PHR.mp3" },
+    { title: "Zip FM", url: "http://84.46.205.13:80/zipfm128.mp3" }
   ],
   active: null,
   playing: false,
